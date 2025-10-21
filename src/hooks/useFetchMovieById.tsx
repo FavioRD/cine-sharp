d1ec3c734566944dc20299ddf426c72deaa1cdc8
Jerucho <amadorjeremias2006@gmail.com>
2025-10-21 17:53:29 -0500

    fix: Si no hay detalle de pelicula, redirigir al inicio

--- src/hooks/useFetchMovieById.tsx
+++ src/hooks/useFetchMovieById.tsx
@@ -1,17 +1,24 @@
 import { useState, useEffect } from "react";
 import type { DetalleMovieDTO } from "../dtos/MovieDTO";
+import { useNavigate } from "react-router-dom";
 
 export const useFetchMovieById = (id: number) => {
-    const [movie, setMovie] = useState<DetalleMovieDTO>();
-    const url = import.meta.env.VITE_API_URL;
-    useEffect(() => {
+  const [movie, setMovie] = useState<DetalleMovieDTO>();
+  const url = import.meta.env.VITE_API_URL;
+  const navigate = useNavigate();
+  useEffect(() => {
     const fetchMovie = async () => {
-        const response = await fetch(`${url}/peliculas/detalle/${id}`);
-        const data = await response.json();
+      const response = await fetch(`${url}/peliculas/detalle/${id}`);
+      const data = await response.json();
+
+      if (response.status === 200) {
         setMovie(data);
+      } else {
+        navigate("/404");
+      }
     };
     fetchMovie();
-    }, [id, url]);
+  }, [id, url, navigate]);
 
-    return { movie };
+  return { movie };
 };
